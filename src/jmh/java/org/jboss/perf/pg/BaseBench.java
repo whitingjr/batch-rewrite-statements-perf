f30c4497dbcb714c891a454f935b654aa72aeb23
Jeremy Whiting <jeremy.whiting@hushmail.com>
2015-03-04 18:16:57 +0000

    Fixed benchmark defects. Corrected binding of parameters. Organized properties files. Finished property loading and overrides. Configured jmh jvm command line options to include properties in gradle.properties. Added 2 more benchmark operations (not complete).

--- src/jmh/java/org/jboss/perf/pg/BaseBench.java
+++ src/jmh/java/org/jboss/perf/pg/BaseBench.java
@@ -1,34 +1,31 @@
 package org.jboss.perf.pg;
 
 import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Statement;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
 import org.apache.tomcat.jdbc.pool.PoolProperties;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-
-import com.sun.xml.internal.fastinfoset.sax.Properties;
 
 public class BaseBench {
 
    protected static final int MIN_POOL_SIZE = 1;
    
    protected static volatile DataSource ds;
+   private static Logger logger = Logger.getLogger(BaseBench.class.getName());
    
    public void createPool()
    {
-      try {
+      try 
+      {
          PoolProperties props = new PoolProperties();
-         props.setUrl("jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=false&prepareThreshold=1&tcpKeepAlive=true");
-         props.setDriverClassName("org.apache.tomcat.jdbc.pool.DataSource");
+         props.setUrl(String.format("jdbc:postgresql://%1$s/%2$s?ssl=false&sslmode=disable&prepareThreshold=1&tcpKeepAlive=%3$s", System.getProperty("host"), System.getProperty("database"), System.getProperty("keepalive")));
+         props.setDriverClassName("org.postgresql.Driver");
          props.setUsername(System.getProperty("user"));
          props.setPassword(System.getProperty("password"));
          props.setInitialSize(MIN_POOL_SIZE);
@@ -38,12 +35,12 @@ public class BaseBench {
          props.setMaxActive(Integer.valueOf(max).intValue());
          props.setMaxWait(8000);
          props.setDefaultAutoCommit(false);
-         props.setRollbackOnReturn(true);
+         props.setRollbackOnReturn(false);
          props.setMinEvictableIdleTimeMillis((int) TimeUnit.MINUTES.toMillis(30));
          props.setTestOnBorrow(true);
          props.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-         props.setValidationQuery("VALUES 1");
-         props.setJdbcInterceptors("org.apache.tomcat.jdbc.pool.interceptor.StatementCache(prepared=true,max=16)");
+         props.setValidationQuery("SELECT 1");
+         props.setJdbcInterceptors("org.apache.tomcat.jdbc.pool.interceptor.StatementCache(prepared=true,max=8)");
          ds = new org.apache.tomcat.jdbc.pool.DataSource(props);
       }
       catch (Exception e)
@@ -57,10 +54,20 @@ public class BaseBench {
       ((org.apache.tomcat.jdbc.pool.DataSource) ds).close();
    }
 
-   public void loadProperties()
+   /**
+    * Properties loading from configuration file. Properties passed to jvm will
+    *  override configuration file settings.
+    */
+   public static void loadProperties()
    {
       Properties props = new Properties();
-      props.load( new BufferedInputStream(  ClassLoader.getSystemResourceAsStream("bench.properties") ) );
-      
+      try {
+         props.load( new BufferedInputStream(  ClassLoader.getSystemResourceAsStream("bench.properties") ) );
+      } catch (IOException ioe)
+      {
+         logger.log(Level.SEVERE, ioe.getMessage());
+      }
+      props.putAll(System.getProperties()); //overwrite
+      System.getProperties().putAll(props);
    }
 }
--- src/jmh/java/org/jboss/perf/pg/MultiInsertStatementTest.java
+++ src/jmh/java/org/jboss/perf/pg/MultiInsertStatementTest.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 import org.apache.commons.dbutils.DbUtils;
 import org.jboss.perf.BatchSQLEnum;
@@ -22,24 +23,41 @@ import org.openjdk.jmh.annotations.TearDown;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
-@Measurement(iterations=100, timeUnit=TimeUnit.MINUTES)
 public class MultiInsertStatementTest extends BaseBench {
    
    @Benchmark
-   public void do1MultirowInserts( ThreadState state, MultiRowBenchmarkState benchmarkState ) throws SQLException
+   @Measurement(iterations=100, timeUnit=TimeUnit.MINUTES)
+   public void do1MultirowInserts( ThreadState state, MultiRowBenchmarkState benchmarkState )
    {  
-      executeAsBatch(state, benchmarkState.SMALL);
+      try {
+         executeAsBatch(state, benchmarkState.SMALL);
+      } catch (SQLException sqle )
+      {
+         throw new RuntimeException(sqle.getMessage() + " cause :" + sqle.getCause().getMessage());
+      }
    }
    
-//   @Benchmark
-//   public void do11MultirowInserts( ThreadState state )
-//   {
-//   }
-//   
-//   @Benchmark
-//   public void do51MultirowInserts( ThreadState state )
-//   {
-//   }
+   @Benchmark
+   public void do11MultirowInserts( ThreadState state, MultiRowBenchmarkState benchmarkState  )
+   {
+      try {
+         executeAsBatch(state, benchmarkState.MEDIUM);
+      } catch (SQLException sqle )
+      {
+         throw new RuntimeException(sqle.getMessage() + " cause :" + sqle.getCause().getMessage());
+      }
+   }
+   
+   @Benchmark
+   public void do51MultirowInserts( ThreadState state, MultiRowBenchmarkState benchmarkState )
+   {
+      try {
+         executeAsBatch(state, benchmarkState.LARGE);
+      } catch (SQLException sqle )
+      {
+         throw new RuntimeException(sqle.getMessage() + " cause :" + sqle.getCause().getMessage());
+      }
+   }
 
    /** 
     * Benchmark state that is set up once beforehand.
@@ -51,6 +69,7 @@ public class MultiInsertStatementTest extends BaseBench {
       BatchSQLEnum SMALL = BatchSQLEnum.SMALL;
       BatchSQLEnum MEDIUM = BatchSQLEnum.MEDIUM;
       BatchSQLEnum LARGE = BatchSQLEnum.LARGE;
+      private static Logger logger = Logger.getLogger(MultiRowBenchmarkState.class.getName());
       
       @Setup (Level.Trial)
       public void setUpSQL() 
@@ -59,23 +78,29 @@ public class MultiInsertStatementTest extends BaseBench {
          MEDIUM.setCount(getSize("medium.batch.size"));
          LARGE.setCount(getSize("large.batch.size"));
          
-         buildSQL(SMALL);
-         buildSQL(MEDIUM);
-         buildSQL(LARGE);
+         SMALL.setSQL( buildSQL(SMALL) );
+         MEDIUM.setSQL( buildSQL(MEDIUM) );
+         LARGE.setSQL( buildSQL(LARGE) );
          Connection c = null;
          Statement s = null;
          try 
          {
             c = ds.getConnection();
+            c.setAutoCommit(false);
             s = c.createStatement();
-            s.execute("drop table orderline if exists");
-            s.close();
+            s.execute("DROP TABLE IF EXISTS orderline;");
+            DbUtils.closeQuietly( s );
             s = c.createStatement();
-            s.execute("create table orderline ( orderLineId bigint not null, description varchar (20) );");
-            
-         } catch ( SQLException sqle ) {
-            DbUtils.closeQuietly( s );  
-            DbUtils.closeQuietly( c );  
+            s.execute("CREATE TABLE orderline ( orderLineId bigint NOT NULL, description VARCHAR (20), PRIMARY KEY (orderLineId) );");
+            c.commit();
+         } catch ( SQLException sqle ) 
+         {
+            logger.log(java.util.logging.Level.SEVERE, sqle.getMessage());
+         }
+         finally 
+         {
+            DbUtils.closeQuietly( s );
+            DbUtils.closeQuietly( c );
          }
       }
       protected String buildSQL(BatchSQLEnum sql) 
@@ -95,7 +120,7 @@ public class MultiInsertStatementTest extends BaseBench {
       private long getSize(String key)
       {
          return Long.parseLong(System.getProperty(key));
-      }      
+      }
    }
    
    /**
@@ -107,36 +132,46 @@ public class MultiInsertStatementTest extends BaseBench {
    {
       Connection conn;
       volatile int iteration = 0;
-      
+
       @Setup (Level.Invocation)
       public void setUp() throws SQLException
       {
-         this.conn = ds.getConnection(); 
+         this.conn = ds.getConnection();
+         this.conn.setAutoCommit(false);
       }
       @TearDown (Level.Invocation)
       public void tearDown() throws SQLException
       {
          this.conn.close();
+         iteration += 1;
       }
    }
    
+   /**
+    * Bind the values to each position in the statement.
+    * @param ps
+    * @param sql
+    * @param state
+    * @return
+    * @throws SQLException
+    */
    private PreparedStatement bindValues( PreparedStatement ps, BatchSQLEnum sql, ThreadState state ) throws SQLException
    {
       long count = sql.getCount();
       long initialId = state.iteration * sql.getCount(); 
       int pos = 1;
-      for (long c = 1l; c < count ; c += 1l  )
+      for (long c = 0l; c < count ; c += 1l  )
       {
          ps.setLong(pos, initialId + c);
          pos += 1;
          ps.setString(pos, Long.toString(c));
          pos += 1;
-      } 
+      }
       return ps;
    }
    
    /**
-    * Execute the sql batch with the multi row insert. 
+    * Execute the sql batch with the multi row insert.
     * @param state
     * @param sql enum for the batch size
     * @return the total number of rows inserted
@@ -150,7 +185,9 @@ public class MultiInsertStatementTest extends BaseBench {
       {
          ps = state.conn.prepareStatement(sql.getSQL());
          bindValues(ps, sql, state);
+         ps.addBatch();
          int uc[] = ps.executeBatch();
+         state.conn.commit();
          for (int c: uc)
          {
             rv += c;
@@ -165,6 +202,7 @@ public class MultiInsertStatementTest extends BaseBench {
    @Setup
    public void setUp()
    {
+      MultiInsertStatementTest.loadProperties();
       super.createPool();
    }
    @TearDown
